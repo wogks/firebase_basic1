aca26096ae6f7b786e1976ceef1a7718ca518f3e
wogks <wogks27@naver.com>
2022-11-05 02:20:29 +0900

    Feat: go_router

---
+++ lib/presentation/routes/routes.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+final GoRouter _router = GoRouter(
+    routes: <GoRoute>[
+      GoRoute(
+        path: '/',
+        builder: (BuildContext context, GoRouterState state) {
+          return ScreenA();
+        },
+      ),
+      GoRoute(
+        path: '/b',
+        builder: (BuildContext context, GoRouterState state) {
+          return ScreenB();
+        },
+      ),
+    ],
+  );
